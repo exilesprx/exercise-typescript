0df8f1b38ba8140d3a1f703bcc82fb815ee55966
Exiles.prx <exiles.prx@gmail.com>
2024-06-29 17:03:12 -0400

    Solved exercise luhn

--- luhn/luhn.ts
+++ luhn/luhn.ts
@@ -1,3 +1,23 @@
-export function valid(digitString: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function valid(digitString: string): boolean {
+  const digits = digitString.replace(/\s/g, "");
+
+  if (!/^\d+$/.test(digits)) {
+    return false;
+  }
+
+  let sum = 0;
+  let digitCount = 0;
+  for (let i = digits.length - 1; i >= 0; i--) {
+    digitCount++;
+    let digit = parseInt(digits[i]);
+    if (digitCount % 2 === 0) {
+      digit *= 2;
+      if (digit > 9) {
+        digit -= 9;
+      }
+    }
+    sum += digit;
+  }
+
+  return digitCount > 1 && sum % 10 === 0;
 }
