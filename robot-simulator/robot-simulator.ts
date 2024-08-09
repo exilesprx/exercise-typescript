edf9d442503e2805005cfb75330af92cbad951e7
Exiles.prx <exiles.prx@gmail.com>
2024-08-09 13:38:16 -0400

    Implement base functions

--- robot-simulator/robot-simulator.ts
+++ robot-simulator/robot-simulator.ts
@@ -9,16 +9,20 @@ type Direction = 'north' | 'east' | 'south' | 'west'
 type Coordinates = [number, number]
 
 export class Robot {
+  private direction: Direction = 'north'
+  private coords: Coordinates = [0, 0];
+
   get bearing(): Direction {
-    throw new Error('Remove this statement and implement this function')
+    return this.direction;
   }
 
   get coordinates(): Coordinates {
-    throw new Error('Remove this statement and implement this function')
+    return this.coords;
   }
 
-  place({}: { x: number; y: number; direction: string }) {
-    throw new Error('Remove this statement and implement this function')
+  place({x, y, direction}: { x: number; y: number; direction: string }) {
+    this.coords = [x, y];
+    this.direction = direction as Direction;
   }
 
   evaluate(instructions: string) {
